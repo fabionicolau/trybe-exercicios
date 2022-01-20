ebd1ea138f1afe4109e80e210934b71f2b8b6225
fabionicolau <fabionicolaujr@gmail.com>
2022-01-20 15:02:33 -0300

    exercicios/8.2

---
+++ modulo-01-fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/some&every.js
@@ -0,0 +1,22 @@
+const names = ['Mateus', 'José', 'Ana', 'Cláudia', 'Bruna'];
+
+const hasName = (arr, name) => arr.some((nome) => nome === name)
+
+
+console.log(hasName(names, 'Ana'))
+
+//
+
+const people = [
+  { name: 'Mateus', age: 18 },
+  { name: 'José', age: 16 },
+  { name: 'Ana', age: 23 },
+  { name: 'Cláudia', age: 20 },
+  { name: 'Bruna', age: 19 },
+];
+
+
+const verifyAges = (arr, minimumAge) => arr.every((pessoa) => pessoa.age >= minimumAge)
+
+
+console.log(verifyAges(people, 18));
