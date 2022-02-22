3ce88f91b6c771bc5fb5140e00776d5b8a2855f7
fabionicolau <fabionicolaujr@gmail.com>
2022-02-22 14:49:48 -0300

    refatorando componente button

--- modulo-02-front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/my-pokedex/src/Button.jsx
+++ modulo-02-front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/my-pokedex/src/Button.jsx
@@ -1,20 +1,6 @@
 import React from 'react'
 
 class Button extends React.Component {
-  constructor() {
-    super()
-    this.state = {
-    filter: 'all'   
-    }
-  }
-
- handleType = (name) => {
-    this.setState((estadoAtual, _props) => ({
-      pokeIndex: 0,
-      filter: estadoAtual.filter = name
-    }))
-  }
-
   render() {
     const { filter, handleType } = this.props
     return (
